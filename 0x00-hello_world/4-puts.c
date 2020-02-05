36197f34f3f304017e011ecb89dd2b2688511fb2
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-05 18:18:11 +0000

    Added betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - this function runs this program
+ * Return: printf 0 if everything is well
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
