e2ef581c303d065733a11da7e36bbeef20d6f504
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-05 19:55:56 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - this is the main function
+ * Return: always return (0)
+ */
 int main(void){
-  
+  printf("Size of a char: %2d\n", sizeof(char));
+  printf("Size of an int: %2d\n", sizeof(int));
+  printf("Size of a long int: %2d\n", sizeof(long int));
+  printf("Size of a long long int: %2d\n", sizeof(long int *));
+  printf("Size of a float: %2d\n", sizeof(float));
   return 0;
 }
