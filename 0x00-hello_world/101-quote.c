f40c8f0349f817b51fc91ef56e41cfe34ea6ef29
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-05 21:42:26 +0000

    Advanced 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ * main - this is the main function
+ * Return: Will be 1
+*/
+int main(void)
+{
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 60;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ( "syscall" : "=a" (ret) : "a" (syscall), "D" (fd),"S" (s), "d" (l));
+return (1);
+}
