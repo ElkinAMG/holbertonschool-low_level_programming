dff66ef0ab6c1ea23ceb6c4f550c8e68f2170f3d
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 21:12:21 +0000

    Task 11

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return - 0 always
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+putchar(a);
+putchar(b);
+if (a == 57 && b == 57)
+{
+break;
+}
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
