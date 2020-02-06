2e89ca8a0440a19f1c6503660ac5e9732d960f3b
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 19:43:43 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: 0 always
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
