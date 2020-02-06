05ef5db2bf7ffe97fb4e1c9d6c4927d3fc1f31df
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 04:52:36 +0000

    Advanceed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,14 @@
 /**
  * main - this is the main function
  * Return: Will be 1
-*/
+ */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1,sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),stdout);
-return (1);
+  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  long l = 59;
+  long fd = 1;
+  long syscall = 1;
+  long ret = 0;
+  __asm__ ("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
+  return (1);
 }
