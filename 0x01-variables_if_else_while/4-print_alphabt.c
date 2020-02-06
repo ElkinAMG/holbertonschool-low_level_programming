2abbf76ed8ec4ee2e212e30f0b4d0ea5337a8006
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 19:20:43 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
