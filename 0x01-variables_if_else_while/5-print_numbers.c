ba2ea08856b3bf69372b55500794d3b47492f424
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 19:27:17 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: 0 always
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%i", i);
+i++;
+}
+putchar('\n');
+return (0);
+}
