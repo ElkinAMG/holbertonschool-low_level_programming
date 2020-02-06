1b7a55ecd07a950573bdaade90235068c3bb4a6d
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 04:51:03 +0000

    Advanceed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,6 @@
 */
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall" : "=a" (ret) : "a" (syscall), "D" (fd), "S" (s), "d" (l));
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1,sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"),stdout);
 return (1);
 }
