957ca2c12349f34e90c608ac27d99ea56a14f64b
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-06 19:34:48 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always will be 0
+ */
+int main(void)
+{
+int i;
+for(i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
