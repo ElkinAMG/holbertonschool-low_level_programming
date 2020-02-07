534483e570c31691837358699208be10fa7eee77
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-07 03:14:44 +0000

    Advanced 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 int a = '0';
 int b;
 int c;
-while(a <= '9')
+while (a <= '9')
 {
 b = a + 1;
-while(b <= '9')
+while (b <= '9')
 {
 c = b + 1;
-while(c <= '9')
+while (c <= '9')
 {
 putchar(a);
 putchar(b);
 putchar(c);
-if(a == '7' && b == '8' && c == '9')
+if (a == '7' && b == '8' && c == '9')
 {
 break;
 }
