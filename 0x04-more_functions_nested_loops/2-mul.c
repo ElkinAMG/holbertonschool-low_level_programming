72a24ff3c1c33b767d4958adf5d0cf7f6806496c
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-13 10:58:53 +0000

    3rd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - Writes the multiplication between two variables
+ * @a: first variable
+ * @b: second variable
+ *
+ * Return: return the value of a by b
+ */
+int mul(int a, int b)
+{
+	return ((a*b));
+}
