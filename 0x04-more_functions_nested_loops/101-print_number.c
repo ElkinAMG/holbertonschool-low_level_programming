b6640053c3b5ee2e12fb25f1022459bb5dd30bcc
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-14 04:59:02 +0000

    Test, Advanced 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,34 +5,13 @@
  */
 void print_number(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
-		if (n != 0)
-		{
 		_putchar('-');
 		n = (n * (-1));
-		}
-		else
-		{
-		_putchar('0');
-		}
 	}
-	if (n > 999)
-	{
-		_putchar((n / 100) / 10 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar((n % 100) % 10 + '0');
-	}
-	else if (n > 99)
-	{
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 100) / 10 + '0');
-		_putchar((n % 100) % 10 + '0');
-	}
-	else if (n > 9)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
