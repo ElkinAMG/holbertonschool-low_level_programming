423215d9790f78d87b3f8386cdd232f7b8d11fb3
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-17 10:23:48 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - takes the current value of n and change it to 98.
+ * @n: this variable will be the argument for this function.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
