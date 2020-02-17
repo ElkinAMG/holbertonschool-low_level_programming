2a15746bf73ce092c5f52f3011500e1b25dd0c70
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-17 09:59:06 +0000

    Starting with pointers

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+int _putchar(char);
+void reset_to_98(int*);
+void swap_int(int*, int*);
+int _strlen(char *s);
+void _puts(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char*);
+void puts_half(char *);
+void print_array(int *, int);
+char *_strcpy(char *, char *);
+int _atoi(char *);
+
+#endif
