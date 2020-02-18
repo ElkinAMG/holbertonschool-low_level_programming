f9aabee83c80a7d895cc3635c765b39ed615163b
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-18 17:28:15 +0000

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,8 +6,24 @@
  */
 int _atoi(char *s)
 {
+  int res, i, sign;
+  sign = 1;
+  res = 0;
 
-	while (*(s++) != '\0')
-		(*(s) >= '0' && *(s) <= '9') ? return (*(s)) : return (0);
-
+  for (i = 0; s[i] != '\0'; ++i)
+    {
+      if ((s[i + 1] >= '0' && s[i + 1] <= '9') && s[i] == '-')
+	{
+	sign = -1;
+	i++;
+	}
+      if (s[i] >= '0' && s[i] <= '9')
+	{ 
+      res = (res * 10 + s[i] - '0');
+      if (s[i + 1] == ' ')
+	break;
+	}
+    }
+  
+  return res * sign;
 }
