629ddd5dc161344c3ed367eabc0af3d1fc332553
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-18 17:36:26 +0000

    Advanced 1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,24 +6,36 @@
  */
 int _atoi(char *s)
 {
-  int res, i, sign;
-  sign = 1;
-  res = 0;
+	int isnum(int num), res, i, sign;
+	sign = 1;
+	res = 0;
 
-  for (i = 0; s[i] != '\0'; ++i)
-    {
-      if ((s[i + 1] >= '0' && s[i + 1] <= '9') && s[i] == '-')
+	for (i = 0; s[i] != '\0'; ++i)
 	{
-	sign = -1;
-	i++;
-	}
-      if (s[i] >= '0' && s[i] <= '9')
-	{ 
-      res = (res * 10 + s[i] - '0');
-      if (s[i + 1] == ' ')
-	break;
-	}
-    }
-  
-  return res * sign;
-}
+		if (isnum(s[i] && s[i] == '-')
+			{
+				sign = -1;
+				i++;
+			}
+			if (isnum(s[i])
+				{
+					res = (res * 10 + s[i] - '0');
+					if (s[i + 1] == ' ')
+						break;
+				}
+				}
+			return (res * sign);
+			}
+/**
+ * isnum - checks if this variable is a number.
+ * @a: variables num
+ *
+ * Return: returns (1) if it's true
+ */
+		int isnum(int a)
+		{
+			if (a >= '0' && a <= '9')
+				return (1);
+			else
+				return (0);
+		}
