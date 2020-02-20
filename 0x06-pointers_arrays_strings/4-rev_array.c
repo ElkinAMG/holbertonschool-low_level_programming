f5bae3d2ada2aaac95de8b988f19ba3a278d472b
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-20 20:41:38 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses the content of an array of 'integers'.
+ * @a: Array.
+ * @n: Elements of the array.
+ */
+void reverse_array(int *a, int n)
+{
+	int aux, i;
+	for (i = 0; i < n / 2; i++)
+	{
+		aux = *(a + i); //Save the index
+		*(a + i) = *(a + n - i - 1); //Swap the ultimate - i with index + i
+		*(a + n - i - 1) = aux;
+	}
+}
