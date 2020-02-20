ebda0b578c52009be7cdd976adc047dbc349c495
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-20 17:55:46 +0000

    Fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int sign, i;
+
 	sign = 1;
+
 	/*substract s1++ with s2++, if total is == 0 break the loop*/
 	for (i = 0; sign != 0 && *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 	{
-		sign = ((*s1 + i) - '0') - ((*s2 + i)- '0');
+		sign = ((*s1 + i) - '0') - ((*s2 + i) - '0');
 	}
 
 	return (sign);
