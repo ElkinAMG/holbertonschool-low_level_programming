058358a843f7365d92fd5184454460f50fa4fa47
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-20 20:45:21 +0000

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,11 @@
 void reverse_array(int *a, int n)
 {
 	int aux, i;
+
 	for (i = 0; i < n / 2; i++)
 	{
-		aux = *(a + i); //Save the index
-		*(a + i) = *(a + n - i - 1); //Swap the ultimate - i with index + i
-		*(a + n - i - 1) = aux;
+		aux = *(a + i); /*Save the index*/
+		*(a + i) = *(a + n - i - 1); /*Swap the ultimate - i with index + i*/
+		*(a + n - i - 1) = aux; /*Swap the first position with the ultimate*/
 	}
 }
