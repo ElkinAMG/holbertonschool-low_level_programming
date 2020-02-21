9a9f9ad105648f0a3095111c54425dfa696e3b1c
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-21 06:15:23 +0000

    Advanced 1

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_number - prins numbers
+ * @n: number value
+ */
+void print_number(int n)
+{
+	int tl;
+	tl = 0;
+
+	if (n == 0)
+		_putchar(0 + '0');
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		n = (n * (-1));
+	}
+
+	while (n != 0)
+	{
+		tl = (tl * 10 + (n % 10));
+		n /= 10;
+	}
+
+	n = tl;
+
+	while (n != 0)
+	{
+		tl = (tl * 10 + (n % 10));
+		_putchar(tl % 10 + '0');
+		n /= 10;
+	}
+
+
+}
