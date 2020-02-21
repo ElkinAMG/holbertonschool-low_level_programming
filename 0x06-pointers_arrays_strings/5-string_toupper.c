8d32e3347f4598a2402b4206a2644d023e22d751
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-02-21 02:27:30 +0000

    Task 7

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *s)
 	int i;
 
 	i = 0;
-
+	/*travel through the string until the end*/
 	while (*(s + i) != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
-			*(s + i) = (*(s + i) - 32);
+			*(s + i) = (*(s + i) - 32); 
 		i++;
 	}
 	return (s);
