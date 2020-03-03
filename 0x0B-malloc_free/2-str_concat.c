b9779abd3f0f1723deeb7672fc9099d2950e051d
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-03 20:24:10 +0000

    Fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	char *concat = NULL;
 
-	(s1 == NULL ? s1 = '' : 0);
-	(s2 == NULL ? s2 = '' : 0);
+	(s1 == NULL ? s1 = "" : 0);
+	(s2 == NULL ? s2 = "" : 0);
 
 	i = 0;
 
