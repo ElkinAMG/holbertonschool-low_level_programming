b47db8277bb8bd521d2e910ca59af1ffe77d6893
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-03 20:34:38 +0000

    Fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
 		i++;
 
-	concat = malloc(sizeof(char) * (i + 1));
+	concat = malloc(sizeof(char *) * (i + 1));
 
 	if (concat == NULL)
 	{
