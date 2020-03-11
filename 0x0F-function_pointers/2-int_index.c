b7808e7049547f3a7b19d198f5e30bb0a622e8ff
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-11 19:54:10 +0000

    Task 3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - Returns the index of the first element for which the cmp
+ *             function does not return 0.
+ * @array: Array to check with de cmp.
+ * @size: It's the size of elements in the array.
+ * @_cmp: Pointer to function cmp.
+ *
+ * Return: It returns the index of the element truth checked, otherwise returns -1.
+ */
+int int_index(int *array, int size, cmp _cmp)
+{
+	int i;
+
+	if (_cmp != NULL && array != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+			if (!_cmp(*(array + i)))
+				continue;
+			else
+				return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,9 +8,11 @@ int _putchar(char c);
 /*TYPEDEFINITIONS*/
 typedef void (*f)(char *);
 typedef void (*action)(int);
+typedef int (*cmp)(int);
 /*END*/
 
 void print_name(char *name, f);
 void array_iterator(int *array, size_t size, action);
+int int_index(int *array, int size, cmp);
 
 #endif /* _FUNCTION_POINTERS_ */
