8ea2a5a15d55cdf2796929d651cd193ad96d4083
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-11 17:46:00 +0000

    0x0F-functions_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - It prints a name.
  * @name: It's name to print.
- * @whats_my_name: Pointer to a function.
+ * @f: Pointer to a function.
  *
  * Return: It does not return nothing.
  */
