562e89ae9e38a41e066c88b904d5e55c7c3c8165
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-19 17:57:04 -0500

    Testing

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,44 +1,5 @@
 #include <string.h>
 #include "lists.h"
-/**
- * add_node - Add a node at the beginning of the LL.
- * @h: Header of the LL.
- * @s: String to add at the new node.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
-
-list_t *add_node(head h, str s)
-{
-	/* Lenght function:  */
-	size_t length(str);
-
-	/* Declare the new node like NULL:  */
-	list_t *newNode = NULL;
-
-	/* Verify: */
-	if (!h || !s)
-		return (NULL);
-
-	/* Define the size of new node:  */
-	newNode = (list_t *)malloc(sizeof(list_t));
-	/* If it fails: */
-	if (!newNode)
-	{
-		free(newNode);
-		return (NULL);
-	}
-	/* Define value of new node -> str:  */
-	newNode->str = strdup(s);
-	/* Define lenght of characters: */
-	newNode->len = length(s);
-	/* Define address to next node: */
-	newNode->next = (*h);
-	/* Redefine the new head for LL: */
-	(*h) = newNode;
-
-	return (newNode->next);
-}
 
 /**
  * length - Calculates the lenght in a string.
@@ -55,4 +16,41 @@ size_t length(str s)
 		i++;
 
 	return (i);
+}
+
+/**
+ * add_node - Add a node at the beginning of the LL.
+ * @h: Header of the LL.
+ * @s: String to add at the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node(head h, str s)
+{
+	/* Declare the new node like NULL:  */
+	list_t *newNode = NULL;
+
+	/* Verify: */
+	if (!h || !s)
+		return (NULL);
+
+	/* Define the size of new node:  */
+	newNode = malloc(sizeof(list_t));
+	/* If it fails: */
+	if (!newNode)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	/* Define value of new node -> str:  */
+	newNode->str = strdup(s);
+	/* Define lenght of characters: */
+	newNode->len = length(s);
+	/* Define address to next node: */
+	newNode->next = (*h);
+	/* Redefine the new head for LL: */
+	(*h) = newNode;
+
+	return (newNode);
 }
