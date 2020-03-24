f40575eab8d2659097bbe18318b7bd8e1a841c66
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-24 05:44:24 -0500

    Getting Fixed x2

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 
 	newNode = (listint_t *)malloc(sizeof(listint_t));
-	if (newNode)
+	if (!newNode)
 		return (NULL);
 
 	newNode->n = n;
