aa2b80c315cf6aab19b817cb889ead4bf6ebb069
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-26 18:42:31 -0500

    Flip Bits Added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - Number of bits to flip for change a number.
+ * @n: Number root.
+ * @m: Number to change to.
+ *
+ * Return: Numbers to flip.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	/*-----------------------------------*/
+	/* n    → 1024 = 0000 0010 0000 0000 */
+	/* m    → 1    = 0000 0000 0000 0001 */
+	/*               ------------------- */
+	/* swap →  ^   = 0000 0010 0000 0001 */
+	/* flips →  &  = 0000 0000 0000 0001 */
+	/*               ------------------- */
+	/*                   FLIPS →  2      */
+	/*-----------------------------------*/
+
+	int swap = n ^ m;
+	int flips = 0;
+
+	for (; swap > 0; flips += (swap & 1), swap >>= 1)
+		;
+
+	return (flips);
+}
