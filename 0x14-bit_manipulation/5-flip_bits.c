be546b4b58bf7c25bc0e9632cd3eacf4f6ec07aa
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-26 18:46:40 -0500

    Testing Fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -22,9 +22,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	/*-----------------------------------*/
 
 	int swap = n ^ m;
-	int flips = 0;
+	int flips;
 
-	for (; swap > 0; flips += (swap & 1), swap >>= 1)
+	for (flips = 0; swap; flips += (swap & 1), swap >>= 1)
 		;
 
 	return (flips);
