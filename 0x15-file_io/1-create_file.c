20c5fb8e856e10da032bb3ceef8b60497cc41716
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-31 14:53:09 -0500

    Fixing Problems

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (wr);
 
-	n_file = creat(filename, S_IRUSR | S_IWUSR);
+	n_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (n_file < 0)
 		return (wr);
 
