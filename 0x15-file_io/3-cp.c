1d7012770f527b4b3ca27f00bdb732b55ac94024
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-03-31 19:40:24 -0500

    Updates Added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		else
+		if (rdF > 0)
 		{
 			wrT = write(opT, txt, rdF);
 			if (wrT < 0)
