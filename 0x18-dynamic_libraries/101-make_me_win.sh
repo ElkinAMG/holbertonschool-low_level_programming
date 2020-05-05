f4b114a333fd69bdf1a0e9efa0e6716d790fd165
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-04 19:29:52 -0500

    Added Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/ElkinAMG/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/rand.so?raw=true
-export LD_PRELOAD=./rand.so
+wget https://github.com/ElkinAMG/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/rand.so?raw=true /tmp
+export LD_PRELOAD=/tmp/rand.so
