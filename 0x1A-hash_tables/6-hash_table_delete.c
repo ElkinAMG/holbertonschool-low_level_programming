977d26628ed389274bb449f938313dd8ac754d0d
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-30 11:22:33 -0500

    Task 6 Up

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,10 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: It's the hash table.
  *
- *
- *
- *
- *
+ * Return: Nothing.
  */
 
 void hash_table_delete(hash_table_t *ht)
@@ -27,4 +26,7 @@ void hash_table_delete(hash_table_t *ht)
 			ht->array[i] = node;
 		}
 	}
+
+	free(ht->array);
+	free(ht);
 }
