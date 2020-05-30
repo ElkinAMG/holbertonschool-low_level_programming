86ebf5354401c1e2252de68e0496437c2c12314f
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-05-30 11:15:31 -0500

    Task 6 Up

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (!ht || !ht->array)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+	}
+}
