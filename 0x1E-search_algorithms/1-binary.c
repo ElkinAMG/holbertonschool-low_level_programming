82dce034d1c647836f1dd51b2c9c1bb0843b5a46
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-08-26 15:04:53 -0500

    Testing BST

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+
+void print_array(int *array, int from, int to)
+{
+	if (from < to)
+		printf("Searching in array: ");
+
+	for (; from < to; from++)
+		printf("%d%s", array[from], from + 1 == to ? "\n" : ", ");
+}
+
+
+/**
+ * binary_search - Description of a function.
+ * @array: A description of a parameter.
+ * @size: A description of a parameter.
+ * @value: A description of a parameter.
+ *
+ * Return: It returns an index, on failure -1.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0, right = (size - 1), index;
+
+	if (array)
+	{
+		print_array(array, 0, size);
+
+		while (left <= right)
+		{
+			index = ((left + right) / 2);
+
+			if (array[index] < value)
+			{
+			        left = index + 1;
+				print_array(array, left, size);
+		        }
+			else if (array[index] > value)
+			{
+		        	right = index - 1;
+				print_array(array, 0, right+1);
+			}
+			else
+				return (index);
+		}
+	}
+
+	return (-1);
+}
